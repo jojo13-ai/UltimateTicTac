1197f3e84136c025f24afade4b57d7c78f01e298
jojo13-ai <dmitry.nikishin.2021@gmail.com>
2024-06-26 19:04:54 +0300

    исправил баг с созданием 2 игрока

--- UltimateTicTac/Models/GameManager.cs
+++ UltimateTicTac/Models/GameManager.cs
@@ -3,25 +3,24 @@ namespace UltimateTicTac.Models;
 public class GameManager
 {
     private Board GameBoard { get; set; }
-    
+
     private Player Player1 { get; set; }
-    
+
     private Player Player2 { get; set; }
 
     public GameManager()
     {
         GameBoard = new Board();
-        
+
         Console.WriteLine("Введите имя 1го игрока");
         Player1 = new Player(Console.ReadLine() ?? "Игрок 1");
-        
-        Player2 = new Player(Console.ReadLine() ?? "Игрок 2");
+
         Console.WriteLine("Введите имя 2го игрока");
+        Player2 = new Player(Console.ReadLine() ?? "Игрок 2");
     }
 
     public void StartGame()
     {
         Console.WriteLine("Game started");
-        
     }
 }
